fe29901f1657699d3591a1993065c8c95f7440da
RaphaCBa9 <raphaelb3@al.insper.edu.br>
2022-05-12 20:43:07 -0300

    loop principal, input de comando

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import funcoes
 
 # aqui é o códio principal, responsável por executar o jogo
+
+print('  ===========================\n||                          ||\n||       Insper Países      ||\n||                          ||\n  ===========================')
+
+# Loop principal
+
+jogo = True
+while jogo:
+    print('comandos:\n \tdica\t- Mostra as dica diponíveis.\n\tinventário\t- Mostra sua posição\n\tDesisto\tdesiste do jogo')
+    comando = input('Digite um comando ou um palpite')
+    #dicas
+
+
+
+    if comando.lower() == 'desisto':
+        jogo = False
