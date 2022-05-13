2f391f0dcea316d5403271fa8e5a11226605534f
RaphaCBa9 <raphaelb3@al.insper.edu.br>
2022-05-13 13:31:15 -0300

    bug de dicas consertado

--- main.py
+++ main.py
@@ -7,41 +7,62 @@ print('  ===========================\n||                          ||\n||       I
 # Loop principal
 tentativas = 0#colocar limitador de 20 tentativas
 jogo = True
+tentativas = 20
 while jogo:
-    print('comandos:\n \tdica\t- Mostra as dica diponíveis.\n\tinventário\t- Mostra sua posição\n\tDesisto\tdesiste do jogo')
+    print('Comandos:\n \tDica\t- Mostra as dica diponíveis.\n\tInventario\t- Mostra sua posição\n\tDesisto\t- desiste do jogo')
     comando = input('Digite um comando ou um palpite: ')
-    #dicas
 
 
-
-    if comando.lower() == 'desisto':
-        jogo = False
+    #       comando de dicas
     if comando.lower() == 'dica': 
         print('Mercado de Dicas \n ----------------------------------------\n\t1. Cor da bandeira  - custa 4 tentativas\n\t2. Letra da capital - custa 3 tentativas\n\t3. Área             - custa 6 tentativas\n\t4. População        - custa 5 tentativas\n\t5. Continente       - custa 7 tentativas\n\t0. Sem dica\n----------------------------------------')
         escolha = int(input('Escolha sua opção [0|1|2|3|4|5]: '))
-        while escolha != 0 or escolha != 1 or escolha != 2 or escolha != 3 or escolha != 4 or escolha != 5:
-            print('Opção inválida')
-            escolha = int(input('Escolha sua opção [0|1|2|3|4|5]: '))
+        while escolha != 0 and escolha != 1 and escolha != 2 and escolha != 3 and escolha != 4 and escolha != 5:
+                print('Opção inválida')
+                escolha = int(input('Escolha sua opção [0|1|2|3|4|5]: '))
         if escolha == 0:
             print('\n\tDistâncias:\n\tDicas:')
-            jogo = True
+
         if escolha == 1:
-            tentativas += 4
-            print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica a cor da bandeira
-            jogo = True
+            tentativas -= 4
+            if tentativas <= 0:
+                print('Você não possue tentativas o suficiente para comprar esta dica!')
+            else:
+                print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica a cor da bandeira
+
         if escolha == 2:
-            tentativas += 3
-            print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica a letra da capital
-            jogo = True
+            tentativas -= 3
+            if tentativas <= 0:
+                print('Você não possue tentativas o suficiente para comprar esta dica!')
+            else:
+                print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica a letra da capital
+
         if escolha == 3:
-            tentativas += 6
-            print('\n\tDistâncias:\n\tDicas:')
-            jogo = True
+            tentativas -= 6
+            if tentativas <= 0:
+                print('Você não possue tentativas o suficiente para comprar esta dica!')
+            else:
+                print('\n\tDistâncias:\n\tDicas:')
+
         if escolha == 4:
-            tentativas += 5
-            print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica a população
-            jogo = True
+            tentativas -= 5
+            if tentativas <= 0:
+                print('Você não possue tentativas o suficiente para comprar esta dica!')
+            else:
+                print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica a população
+
         if escolha == 5:
-            tentativas += 7
-            print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica o continente
-            jogo = True
+            tentativas -= 7
+            if tentativas <= 0:
+                print('Você não possue tentativas o suficiente para comprar esta dica!')
+            else:
+                print('\n\tDistâncias:\n\tDicas:')#adicionar ao lado da dica o continente
+
+
+#           comando de desistência
+
+    if comando.lower() == 'desisto' or tentativas == 0:    
+        jogo = False
+
+
+    
